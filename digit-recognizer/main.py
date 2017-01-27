fcab4b090d3dcc0ff38acb65ad6a4df0da0edb9f
Phil Bazun <Phil9lne@gmail.com>
2017-01-28 03:07:37 +0500

    First version of digit-recognizer (0.94886)

---
+++ digit-recognizer/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pandas as pd
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import np_utils
+
+
+if __name__ == '__main__':
+    np.random.seed(42)
+    (X_train1, y_train1), (X_test1, y_test1) = mnist.load_data()
+
+    train = pd.read_csv('Data/train.csv')
+    labels = train.ix[:, 0].values.astype('int32')
+    X_train = train.ix[:, 1:].values.astype('float32')
+    X_test = pd.read_csv('Data/test.csv').values.astype('float32')
+    y_train = np_utils.to_categorical(labels)
+
+    X_train /= 255
+    X_test /= 255
+
+    model = Sequential()
+
+    model.add(Dense(800, input_dim=784, init="normal", activation="relu"))
+    model.add(Dense(10, init="normal", activation="softmax"))
+
+    model.compile(loss="categorical_crossentropy", optimizer="SGD",
+                  metrics=["accuracy"])
+
+    model.fit(X_train, y_train, batch_size=200, nb_epoch=100,
+              validation_split=0.2, verbose=1)
+
+    predicts = model.predict_classes(X_test, verbose=1)
+
+    pd.DataFrame({"ImageId": range(1, len(predicts) + 1),
+                  "Label": predicts}).to_csv('Data/result.csv', index=False,
+                                             header=True)
